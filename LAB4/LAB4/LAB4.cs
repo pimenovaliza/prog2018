0c3614686612cccc626b780a16992ed16220d37f
pimenovaliza <lisskaaa.pimenova@icloud.com>
2018-04-07 17:53:55 +0500

    LAB4

---
+++ LAB4/LAB4/LAB4.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB4
+{
+    public class HashTable
+
+    {
+
+
+
+        class KeyValuePair
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+
+
+
+        List<List<KeyValuePair>> list; 
+
+
+
+        public HashTable(int size)
+        {
+            list = new List<List<KeyValuePair>>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(new List<KeyValuePair>());
+            }
+        }
+
+
+
+        public void PutPair(object key, object value)
+        {
+            var bucketNo = GetBucketNumber(key);
+            foreach (var keyValuePair in list[bucketNo])
+            {
+                if (Equals(keyValuePair.Key, key))
+                {
+                    keyValuePair.Value = value;
+                    return;
+                }
+            }
+
+            list[bucketNo].Add(new KeyValuePair { Key = key, Value = value });
+        }
+        
+
+
+        public object GetValueByKey(object key)
+        {
+            var BucketNo = GetBucketNumber(key);
+            foreach (var keyValuePair in list[BucketNo])
+            {
+                if (Equals(keyValuePair.Key, key))
+                {
+                    return keyValuePair.Value;
+                }
+            }
+
+            return null;
+        }
+
+
+
+        private int GetBucketNumber(object key)
+        {
+            return Math.Abs(key.GetHashCode()) % list.Count;
+        }
+    }
+}
---
+++ LAB4/TESTS/TESTS.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LAB4;
+
+namespace TESTS
+{
+    [TestClass]
+    public class TESTS
+    {
+        [TestMethod]
+        public void ThreeElements()
+        {
+            var hashTableOfThreeElements = new LAB4.HashTable(3);
+
+            hashTableOfThreeElements.PutPair("5", "Five");
+            hashTableOfThreeElements.PutPair("7", "Seven");
+            hashTableOfThreeElements.PutPair("33", "Thirty three");
+
+            Assert.AreEqual(hashTableOfThreeElements.GetValueByKey("5"), "Five");
+            Assert.AreEqual(hashTableOfThreeElements.GetValueByKey("7"), "Seven");
+            Assert.AreEqual(hashTableOfThreeElements.GetValueByKey("33"), "Thirty three");
+        }
+
+        [TestMethod]
+        public void TwoEquialsElements()
+        {
+            var hashTableOfTwoEquialsElementsht = new LAB4.HashTable(5);
+
+            hashTableOfTwoEquialsElementsht.PutPair("5", "Пять");
+            hashTableOfTwoEquialsElementsht.PutPair("5", "Five");
+
+            Assert.AreEqual(hashTableOfTwoEquialsElementsht.GetValueByKey("5"), "Five");
+        }
+
+        [TestMethod]
+        public void BigElementsTest()
+        {
+            var bigHashTable = new LAB4.HashTable(10000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                bigHashTable.PutPair(i, i + "!");
+            }
+
+            Assert.AreEqual(bigHashTable.GetValueByKey(55), "55!");
+        }
+
+        [TestMethod]
+        public void SearchForMissingElements()
+        {
+            var bigHashTable = new LAB4.HashTable(10000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                bigHashTable.PutPair(i, i + "!");
+            }
+
+            for (int i = 10000; i < 11000; i++)
+            {
+                Assert.AreEqual(bigHashTable.GetValueByKey(i), null);
+            }
+        }
+    }
+}
+
