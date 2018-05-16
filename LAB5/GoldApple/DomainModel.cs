4e58e8c7f4911dbb1b3605ef7666cb23dc317d29
pimenovaliza <lisskaaa.pimenova@icloud.com>
2018-05-16 20:26:23 +0500

    LAB5

---
+++ LAB5/GoldApple/DomainModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace GoldApple
+{
+    /// <summary>
+    /// Информация о товаре
+    /// </summary>
+    public class ProductRequestDto
+    {
+        /// <summary>
+        /// Дата заполнения
+        /// </summary>
+        public DateTime Filled { get; set; }
+
+        /// <summary>
+        /// Название бренда
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Описание продукта
+        /// </summary>
+        public List<Description> Description { get; set; }
+        /// <summary>
+        /// Стоимость и валюта
+        /// </summary>
+        public double Price { get; set; }
+        public Currency Currency { get; set; }
+        /// <summary>
+        /// Требования к продукту
+        /// </summary>
+        public ProductRequirements Product { get; set; }
+    }
+
+
+    public enum Currency
+    {
+        Rubles,
+        Dollars
+    }
+    
+
+    /// <summary>
+    /// Требования к продукту
+    /// </summary>
+    public class ProductRequirements
+    {
+        /// <summary>
+        /// Водостойкое средство
+        /// </summary>
+        public bool WaterproofProduct { get; set; }
+        /// <summary>
+        /// Солнцезащитное средсто
+        /// </summary>
+        public List<SunscreenRequirements> SunscreenProduct { get; set; }
+    }
+
+    /// <summary>
+    /// Требования к защите от солнца
+    /// </summary>
+    public class SunscreenRequirements
+    {
+        /// <summary>
+        /// Степень защиты
+        /// </summary>
+        public int SPF { get; set; }
+    }
+    /// <summary>
+    /// Описание
+    /// </summary>
+    public class Description
+    {
+        /// <summary>
+        /// Бренд
+        /// </summary>
+        public string Brend { get; set; }
+        /// <summary>
+        /// Цвет
+        /// </summary>
+        public string Color { get; set; }
+        /// <summary>
+        /// Объем
+        /// </summary>
+        public double Amount { get; set; }
+        /// <summary>
+        /// Код товара
+        /// </summary>
+        public double Code { get; set; }
+        /// <summary>
+        /// Тип кожи
+        /// </summary>
+        public SkinType Type { get; set; }
+
+    }
+
+    /// <summary>
+    /// Тип кожи
+    /// </summary>
+    public enum SkinType
+    {
+        Any,
+        Dry,
+        Oily,
+        Problem,
+    }
+}
---
+++ LAB5/UnitTestProject1/SeralizationTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using GoldApple;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class SeralizationTests
+    {
+        [TestMethod]
+        public void End2EndSerializationTest()
+        {
+            var dto = new ProductRequestDto
+            {
+                Filled = DateTime.Now,
+                FullName = "Clinique Face Cream Hybrid SPF30",
+                Price = 599,
+                Currency = Currency.Rubles,
+                Product = new ProductRequirements()
+                {
+                    SunscreenProduct = new List<SunscreenRequirements>()
+                    {
+                        new SunscreenRequirements() { SPF = 30 },
+                        new SunscreenRequirements() { SPF = 50 },
+                    },
+                    WaterproofProduct = false
+                },
+                Description = new List<Description>()
+                {
+                    new Description
+                    {
+                        Brend = "Clinique",
+                        Color = "White",
+                        Amount = 30,
+                        Code = 354720,
+                        Type = SkinType.Any
+                    },
+                    new Description
+                    {
+                        Brend = "Clinique",
+                        Amount = 20,
+                        Code = 1643742,
+                        Type = SkinType.Problem
+                    },
+                }
+            };
+            var tempFileName = Path.GetTempFileName();
+            try
+            {
+                ProductDtoHelper.WriteToFile(tempFileName, dto);
+                var readDto = ProductDtoHelper.LoadFromFile(tempFileName);
+                Assert.AreEqual(dto.Description.Count, readDto.Description.Count);
+                Assert.AreEqual(dto.Filled, readDto.Filled);
+            }
+            finally
+            {
+                File.Delete(tempFileName);
+            }
+        }
+    }
+}
